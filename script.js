2be418a14a7050348987b917082102c9001d7224
DeLightnNH <nikita2000nic@gmail.com>
2024-04-19 09:37:57 +0300

    ДЗ

--- script.js
+++ script.js
@@ -1,64 +1,55 @@
-// Для отримання випадкового числа від 0 до (size-1)
-function getRandomNumber(size) {
-    // Math.random()  -->  [0,1)
-    // [0,1) * 5 = [0*5,1*5) -->  [0;5)
-    // [0,1) * size = [0*size,1*size) -->  [0;size)
-    // Math.random() * size -->  [0;size)
-    let num = Math.floor(Math.random() * size);
-    return num;
-}
+// function func1(num1, num2, num3 = 1, num4 = num1){
+//     num1 *= num4;
+//     num3 += 1;
+//     if(num2 === num3){
+//         console.log(num1);
+//         return;
+//     }
+//     func1(num1, num2, num3, num4);
+// }
 
-// Розраховує відстань від кліку (event) до скарбу (target)
-function getDistance(event, target) {
-    const diffX = event.offsetX - target.x;
-    const diffY = event.offsetY - target.y;
-    const dist = Math.sqrt((diffX * diffX) + (diffY * diffY));
-    return dist;
-}
+// func1(2, 4);
 
-// Задання відстані для отримання підсказки
-function getDistanceHint(distance) {
-    if (distance < 10) {
-        return "Пече!";
-    } else if (distance < 20) {
-        return "Дуже гаряче";
-    } else if (distance < 40) {
-        return "Гаряче";
-    } else if (distance < 80) {
-        return "Тепло";
-    } else if (distance < 160) {
-        return "Холодно";
-    } else if (distance < 320) {
-        return "Дуже холодно";
-    } else {
-        return "Можна замерзнути!";
+document.open();
+
+let number = Number(prompt("Введите число:"));
+let number2 = Number(prompt("Введите предел числа"));
+func1(number);
+
+function func1(num1, num2) {
+    if (num1 > num2) {
+        document.close();
+        return;
     }
+    num1 +=1
+    if(num1 % 2 === 0){
+        document.write(`<h1>${num1}</h1>`);
+    }
+    func1(num1, num2);
 }
 
-// Створюємо змінні
-const width = 450;
-const height = 400;
-let click = 0; // для підрахунку кількості кліків
-const maxMoves = 10
 
-// Випадкове розміщення скарбу
-const target = {
-    x: getRandomNumber(width),
-    y: getRandomNumber(height)
-}
+// function func3 (element) {
+//     console.log(element.number)
+//     if (element.next) {
+//         func3(element.next)
+//      }
+// }
 
-// Додаємо елементу #map обробник кліку
-const mapElement = document.getElementById("map");
-mapElement.addEventListener("click", (event) => {
-    click++; // click = click + 1
-    const answer = getDistanceHint(getDistance(event, target))
-    document.getElementById("distance").innerHTML = `Результат: ${getDistanceHint(getDistance(event, target))}`
-    document.getElementById("counter").innerHTML = `Кількість ходів: ${click}`;
-    if (answer === "Пече!") {
-        alert("gg")
-    }
 
-    if (click === maxMoves ) {
-        alert("ezezezezez looseeer")
-    }
-});
+// const obj = {
+//     number: 1,
+//     next: {
+//         number: 2,
+//         next: {
+//             number: 3,
+//             next: {
+//                 number: 4,
+//                 next: {
+//                     number 5,
+//                 }
+//             }
+//         }
+//     }
+// }
+// func3(obj)
